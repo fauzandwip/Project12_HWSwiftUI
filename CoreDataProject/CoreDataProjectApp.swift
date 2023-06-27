5ed965d3c683897afe5cc9e24d5456e4ae31641b
fauzandp <fauzan27dp@gmail.com>
2023-06-27 23:21:35 +0700

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Fauzan Dwi Prasetyo on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
