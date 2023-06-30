7be187ed9fd0260d5c4a6626f4b662575a6f75a9
fauzandp <fauzan27dp@gmail.com>
2023-06-30 15:05:46 +0700

    Project12_HWSwiftUI | Core Data Introduction Creating NSManagedObject subclasses With computed properties to handle optional

---
+++ CoreDataProject/Introduction/NSManagedObject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Fauzan Dwi Prasetyo on 30/06/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
